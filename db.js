bcbe3d7c549b78787217875caa997bd13262e84e
bigblackclock <abdlhadi1999apdo@gmail.com>
2019-08-26 23:09:40 +0200

    bruh

--- db.js
+++ db.js
@@ -4,19 +4,65 @@ const MongoClient = require('mongodb').MongoClient.connect(url, { useNewUrlParse
 
 
 
-function lastping(){
-    let list=[];
-    let activeList=[]
-    let dissctiveList=[]
-    return new Promise((resolve,reject)=>{
+function lastping() {
+    let list = [];
+    let activeList = []
+    let profiles = []
+    let timestamp = '';
+    let devices=[]
+    return new Promise((resolve, reject) => {
         MongoClient.then((db) => {
-            db.db('nLogs').collection('lastPing').find().toArray().then((doc) => {
-           //   console.log(doc);
-                resolve(doc[0].lastPing)
-              
+            db.db('nLog3').collection('profiles').find().toArray().then((doc) => {
 
+                for (i in doc) {
+                    delete doc[i]._id
+                    delete doc[i].firstSeen
+                    delete doc[i].lastSeen
+                    doc[i].status = false
+                    profiles.push(doc[i])
+                }
+
+
+            }).then(() => {
+
+                db.db('nLog3').collection('lastPing').find().toArray().then((doc) => {
+                    timestamp = doc[0].lastPing.time
+                    console.log(timestamp);
+                    activeList = doc[0].lastPing.devices
+                    for (i in activeList) {
+                        for (x in profiles) {
+                            if (profiles[x].mac === activeList[i].mac) {
+                                list.push(x);
+                                console.log(x);
+                                list.sort(function (a, b) { return b - a })
+
+                            } 
+                        }
+
+                    }
+
+                    for (k in list) {
+                        profiles.splice(list[k], 1)
+                    }
+                    for (l in activeList) {
+                        profiles.push(activeList[l])
+                    }
+
+                    for (d in profiles) {
+                        const sorted = {};
+                        Object.keys(profiles[d]).sort().forEach(function (key) {
+                            sorted[key] = profiles[d][key];
+                        });
+                        devices.push(sorted)
+                    }
+                    resolve({ timestamp: timestamp, devices: devices });
+
+
+
+                })
             })
+
         })
     })
 }
-module.exports.lastPing=lastping
+module.exports.lastPing = lastping
